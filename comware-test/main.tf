424ee806a63ea065821ed17f378fab955ed9d9a9
ParticleG <particle_g@outlook.com>
2025-09-10 23:19:06 +0800

    - Update sources

--- comware-test/main.tf
+++ comware-test/main.tf
@@ -159,7 +159,7 @@ resource "coder_script" "start_code_server" {
     code-server --install-extension "redhat.vscode-xml@prerelease"
     code-server --install-extension "rsbondi.highlight-words"
 
-    code-server --auth none --disable-workspace-trust --locale zh-cn --port 1333 --trusted-origins * &
+    code-server --auth none --disable-workspace-trust --locale zh-cn --port 13337 --trusted-origins * &
   EOF
 }
 
